4f19dfee79c4583f380e5298799ec176bb1e53cc
Kirti <pahwakirti09942@gmail.com>
2022-01-31 22:23:21 +0530

    added volume slider and readme

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <YT
-        sourceUrl="https://res.cloudinary.com/duckchat/video/upload/v1637597453/samples/sea-turtle.mp4"
+        sourceUrl="https://res.cloudinary.com/duckchat/video/upload/v1643380625/samples/animals/wwe-post-CZP3D6WNfE9_iubony.mp4"
         createObjectUrl={true}
       />
     </div>
--- src/components/players/YT.tsx
+++ src/components/players/YT.tsx
@@ -1,10 +1,17 @@
 import React, { useCallback, useEffect, useState, useRef } from "react";
 import { playerProps } from "../../types";
 import { throwPlayerPropsError } from "../../utils/helpers";
+import './YT.scss';
+import {BsFillVolumeDownFill,BsFillVolumeUpFill,BsFillVolumeMuteFill,BsPlayFill,BsPauseFill} from 'react-icons/bs';
+import {BiFullscreen} from 'react-icons/bi';
 
 const YT: React.FC<playerProps> = ({ sourceUrl, createObjectUrl = false }) => {
+  const [muted , setMuted] = useState<number>(50);
   const [url, setUrl] = useState<string>("");
+  const [play, setPlay]=useState<boolean>(true);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const [vol,setVol] = useState<number>(50);
+  const [preVol,setPreVol] = useState<number>(50); 
 
   const createUrlObject = useCallback(async () => {
     const response = await fetch(sourceUrl);
@@ -12,25 +19,106 @@ const YT: React.FC<playerProps> = ({ sourceUrl, createObjectUrl = false }) => {
     const _url = URL.createObjectURL(blob);
     setUrl(_url);
   }, [sourceUrl]);
+  
 
   useEffect(() => {
-    if (!sourceUrl) throwPlayerPropsError();
+    if (!sourceUrl) throwPlayerPropsError(); 
     if (!createObjectUrl) return setUrl(sourceUrl);
     createUrlObject();
   }, [sourceUrl, createUrlObject, createObjectUrl]);
 
+const handlePlay = () => {
+  if(play){
+    videoRef.current?.pause();
+    setPlay(false);
+  }else{
+    videoRef.current?.play();
+    setPlay(true);
+  }
+}
+const handleVolume = () => {
+  console.log(videoRef.current?.volume);
+  if(videoRef.current){
+  if(muted<3){
+    if(preVol>3){
+      videoRef.current.volume=preVol/100;
+      setMuted(preVol);
+      setVol(preVol);
+    }else if(preVol<=3){
+      videoRef.current.volume=50/100;
+      setVol(50);
+      setMuted(50);
+    }  
+  }else{
+    videoRef.current.volume=0;
+    setMuted(0);
+    setVol(0);
+  }
+}
+}
+
+const handleVolumeSlider= (e:any) =>{
+  setPreVol(e.target.value);
+  setVol(e.target.value);
+    if(videoRef.current){
+      videoRef.current.volume=parseInt(e.target.value)/100;
+  }
+  setMuted(e.target.value);
+  console.log(preVol);
+}
+
   return (
-    <div>
+<div>
       {url ? (
-        <>
-          <video autoPlay ref={videoRef}>
-            <source src={url} type="video/mp4" />
+        <div className="videoplayer">
+          <video autoPlay ref={videoRef} className="video" >
+            <source src={url} type="video/mp4"  />
           </video>
-          <button onClick={() => videoRef.current?.play()}>Play</button>
-          <button onClick={() => videoRef.current?.pause()}>Pause</button>
-        </>
+          <div className="controls">
+          <div className="playButton">
+            {play ? (
+              <>
+          <div onClick={handlePlay}> <BsPauseFill /></div>
+              </>
+            ):(
+              <>
+             <div onClick={handlePlay}> <BsPlayFill /></div>
+             </>
+            )}
+          </div>
+          <div className="volBox">
+          <div className="volumeButton">
+            {muted<3 ? (
+                <>
+                <div  onClick={handleVolume}><BsFillVolumeMuteFill /></div>
+                </>
+              ):muted<=50?(
+                <>
+                <div onClick={handleVolume}><BsFillVolumeDownFill /></div>
+                </>
+              ):(
+                <>
+                <div onClick={handleVolume}><BsFillVolumeUpFill /></div>
+                </>
+              )
+            }
+          </div>
+          <div className="volumeSlider">
+          <input type="range" min="0" max="100" step="1" value={vol} onChange={handleVolumeSlider} className="volRange"/>
+          </div>
+          </div>
+          <div className="fullScreen">
+          <div onClick={() => videoRef.current?.requestFullscreen()}><BiFullscreen /></div>
+          </div>
+
+        </div>
+        </div>
       ) : (
-        <p>Loading video...</p>
+        <div className="loader">
+        <div>
+          <img src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif" />
+        </div>
+        </div>
       )}
     </div>
   );
